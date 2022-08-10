2addd49194db8f5a787a381dfe8228d7269ad62b
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-08-10 22:56:05 +0530

    cleaned-code

--- src/components/About.js
+++ src/components/About.js
@@ -2,19 +2,7 @@ import React, { useContext, useEffect } from "react";
 import noteContext from "../context/Notes/noteContext";
 
 function About() {
-	const a = useContext(noteContext);
-	useEffect(() => {
-		a.updateState_();
-		// eslint-disable-next-line
-	}, []);
-
-	return (
-		<>
-			About
-			{a.State_.name}
-			{console.log(a.State_.name)}
-		</>
-	);
+	return <>About</>;
 }
 
 export default About;
--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -2,24 +2,8 @@ import React, { useState } from "react";
 import noteContext from "./noteContext";
 
 function NoteState(props) {
-	const state_ = {
-		name: "Vg",
-		Class: "5b",
-	};
-	const [State_, setState] = useState(state_);
-	const updateState_ = () => {
-		setTimeout(() => {
-			setState({ name: "Ug", Class: "10b" });
-		}, 1000);
-	};
-
 	return (
-		<noteContext.Provider
-			value={{ state_: state_, State_: State_, updateState_: updateState_ }}
-		>
-			{" "}
-			{props.children}{" "}
-		</noteContext.Provider>
+		<noteContext.Provider value={{}}> {props.children} </noteContext.Provider>
 	);
 }
 
