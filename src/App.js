c74a30ad036c51d197378e4b6ca06e38cac40f64
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-08-10 15:51:15 +0530

    navbar, home, about, react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-	return <div className="App">HII</div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="about" element={<About />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
