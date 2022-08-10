46d0f8d1fd39230e8352f84f044bcc3e0582cf56
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-08-10 23:47:37 +0530

    useLocation, forms, nits

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ function App() {
 			<NoteState>
 				<BrowserRouter>
 					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="about" element={<About />} />
-					</Routes>
+					<div className="container">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="about" element={<About />} />
+						</Routes>
+					</div>
 				</BrowserRouter>
 			</NoteState>
 		</>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+	let location = useLocation();
+
 	return (
 		<>
 			<nav className="navbar navbar-expand-lg bg-light">
@@ -23,12 +25,23 @@ function Navbar() {
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 							<li className="nav-item">
-								<Link className="nav-link active" aria-current="page" to="/">
+								<Link
+									className={`nav-link ${
+										location.pathname === "/" ? "active" : ""
+									}`}
+									aria-current="page"
+									to="/"
+								>
 									Home
 								</Link>
 							</li>
 							<li className="nav-item">
-								<Link className="nav-link" to="/about">
+								<Link
+									className={`nav-link ${
+										location.pathname === "/about" ? "active" : ""
+									}`}
+									to="/about"
+								>
 									About
 								</Link>
 							</li>
